31057628d578ddd5b90c29a6f408236cacd86f32
geeg-nas <geovananst77@gmail.com>
2023-09-01 20:30:38 -0300

    login finalizado

---
+++ AppRpgEtec/Views/Usuarios/CadastroView.xaml.cs
@@ -0,0 +1,18 @@
+using AppRpgEtec.ViewModels.Usuarios;
+
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class CadastroView : ContentPage
+{
+	UsuarioViewModel viewModel;
+
+
+	public CadastroView()
+	{
+		InitializeComponent();
+
+		viewModel = new UsuarioViewModel();
+		BindingContext = viewModel;
+
+	}
+}
---
+++ AppRpgEtec/Views/Usuarios/LoginView.xaml.cs
@@ -0,0 +1,17 @@
+using AppRpgEtec.ViewModels.Usuarios;
+
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class LoginView : ContentPage
+{
+	UsuarioViewModel usuarioViewModel;
+
+	public LoginView()
+	{
+		InitializeComponent();
+	
+		usuarioViewModel = new UsuarioViewModel();
+		BindingContext = usuarioViewModel;
+	
+	}
+}
